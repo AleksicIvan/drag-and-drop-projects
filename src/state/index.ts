b5f9b93c6465c1eb0acb5ddf94d34b6797a0c3c6
Ivan Aleksic <ivan.aleksic@guidefitter.com>
2021-01-10 11:22:18 +0100

    Add webpack to project

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,5 +1,5 @@
-import { Project } from '../models/project.js'
-import { ProjectStatus } from '../models/project.js'
+import { Project } from '../models/project'
+import { ProjectStatus } from '../models/project'
 
 // APP STATE
 type Listener<T> = (items: T[]) => void;
---
+++ webpack.config.prod.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const CleanPlugin = require('webpack-clean-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [
+    new CleanPlugin()
+  ]
+};
